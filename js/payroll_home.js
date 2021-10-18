161d6a7ddc9b03bf3b6136298276a8337f49f959
Senthil Kumar S A <sasenthilk@gmail.com>
2021-10-19 05:27:35 +0530

    Added: To remove employee record via json server

--- js/payroll_home.js
+++ js/payroll_home.js
@@ -1,4 +1,4 @@
-let empPayrollList;
+let employeePayrollList;
 window.addEventListener('DOMContentLoaded', (event) => {
     if (site_properties.use_local_storage.match("true")) {
         getEmployeePayrollDataFromStorage();
@@ -8,12 +8,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
 });
 
 const processEmployeePayrollDataResponse = () => {
-    document.querySelector(".emp-count").textContent = empPayrollList.length;
+    document.querySelector(".emp-count").textContent = employeePayrollList.length;
     createInnerHtml();
 }
 
 const getEmployeePayrollDataFromStorage = (key) => {
-    empPayrollList = localStorage.getItem("EmployeePayrollList") ?
+    employeePayrollList = localStorage.getItem("EmployeePayrollList") ?
         JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
     processEmployeePayrollDataResponse();
 }
@@ -21,37 +21,37 @@ const getEmployeePayrollDataFromStorage = (key) => {
 const getEmployeePayrollDataFromServer = () => {
     makeServiceCall("GET", site_properties.server_url, true)
         .then(data => {
-            empPayrollList = JSON.parse(data);
+            employeePayrollList = JSON.parse(data);
             processEmployeePayrollDataResponse();
         }).catch(error => {
             console.log("GET Error Status: " + JSON.stringify(error));
-            empPayrollList = [];
+            employeePayrollList = [];
             processEmployeePayrollDataResponse();
         });
 }
 
 const createInnerHtml = () => {
 
-    if (empPayrollList.length == 0)
+    if (employeePayrollList.length == 0)
         document.querySelector('#table-display').innerHTML = "";
 
     const headerHtml = "<tr><th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th></tr>";
     let innerHtml = `${headerHtml}`;
-    for (const empPayrollData of empPayrollList) {
-        const date = new Date(empPayrollData._startDate);
+    for (const employeePayrollData of employeePayrollList) {
+        const date = new Date(employeePayrollData._startDate);
         const month = date.toLocaleString('default', { month: 'short' });
         let dateString = date.getDate() + " " + month + " " + date.getFullYear();
         innerHtml = `${innerHtml}
         <tr>
-            <td><img class="profile"  src="${empPayrollData._profilePic}" alt=""></td>
-            <td>${empPayrollData._name}</td>
-            <td>${empPayrollData._gender}</td>
-            <td>${(getDeptHtml(empPayrollData._department))}</td>
-            <td>₹ ${empPayrollData._salary}</td>
+            <td><img class="profile"  src="${employeePayrollData._profilePic}" alt=""></td>
+            <td>${employeePayrollData._name}</td>
+            <td>${employeePayrollData._gender}</td>
+            <td>${(getDeptHtml(employeePayrollData._department))}</td>
+            <td>₹ ${employeePayrollData._salary}</td>
             <td>${dateString}</td>
             <td>
-                <img name="${empPayrollData.id}" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
-                <img name="${empPayrollData.id}" alt="edit" onclick="update(this)" src="../assets/icons/create-black-18dp.svg">
+                <img name="${employeePayrollData.id}" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+                <img name="${employeePayrollData.id}" alt="edit" onclick="update(this)" src="../assets/icons/create-black-18dp.svg">
             </td>
         </tr>
         `;
@@ -69,20 +69,31 @@ const getDeptHtml = (deptList) => {
 
 const remove = (node) => {
     deleteIndex = node.parentNode.parentNode.rowIndex;
-    let empPayrollData = empPayrollList.find(employee => deleteIndex == employee.id);
-    if (!empPayrollData) return;
-    const index = empPayrollList.map(employee => employee.id)
-        .indexOf(empPayrollData.id);
-    empPayrollList.splice(index, 1);
-    document.querySelector(".emp-count").textContent = empPayrollList.length;
-    localStorage.setItem("EmployeePayrollList", JSON.stringify(empPayrollList));
-    createInnerHtml();
+    let employeePayrollData = employeePayrollList.find(employee => deleteIndex == employee.id);
+    if (!employeePayrollData) return;
+    const index = employeePayrollList.map(employee => employee.id)
+        .indexOf(employeePayrollData.id);
+    employeePayrollList.splice(index, 1);
+    if (site_properties.use_local_storage.match("true")) {
+        document.querySelector(".emp-count").textContent = employeePayrollList.length;
+        localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
+        createInnerHtml();
+    } else {
+        const deleteURL = site_properties.server_url + employeePayrollData.id.toString();
+        makeServiceCall("DELETE", deleteURL, true)
+            .then(data => {
+                createInnerHtml();
+            })
+            .catch(error => {
+                console.log("DELETE Error Status: " + JSON.stringify(error));
+            });
+    }
 }
 
 const update = (node) => {
     editIndex = node.parentNode.parentNode.rowIndex;
-    let empPayrollData = empPayrollList.find(employee => editIndex == employee.id);
-    if (!empPayrollData) return;
-    localStorage.setItem("EditedEmployeeList", JSON.stringify(empPayrollData));
+    let employeePayrollData = employeePayrollList.find(employee => editIndex == employee.id);
+    if (!employeePayrollData) return;
+    localStorage.setItem("EditedEmployeeList", JSON.stringify(employeePayrollData));
     window.location.replace('../pages/payroll_form.html');
 }
